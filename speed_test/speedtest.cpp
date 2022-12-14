9cac21c55018b40f8c7b41a05893ace6278f4003
Navak94 <44874063+Navak94@users.noreply.github.com>
2022-12-13 23:08:47 -0500

    Add files via upload

---
+++ speed_test/speedtest.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(){
+	printf("start\n");
+	for(long x = 0 ; x < 1000000000; x++){
+		for(long y = 0; y<100000;y++){
+		}
+	}
+	printf("end\n");
+}
